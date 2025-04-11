55f9fc8e4486e3313137239b5c8fc3c15a92cfb3
unknown <eazyisreal192@gmail.com>
2025-04-11 17:04:32 +0100

    fix: Fixed PX on TeamSection

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,4 +1,9 @@
-import { twMerge } from 'tailwind-merge'
+import {
+  GlobeIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon
+} from '@radix-ui/react-icons'
+
 import TeamImg from '../assets/Image.svg'
 import TeamImg1 from '../assets/Image_1.svg'
 import TeamImg2 from '../assets/Image_2.svg'
@@ -7,11 +12,7 @@ import TeamImg4 from '../assets/Image_4.svg'
 import TeamImg5 from '../assets/Image_5.svg'
 import TeamImg6 from '../assets/Image_6.svg'
 import TeamImg7 from '../assets/Image_7.svg'
-import {
-  LinkedInLogoIcon,
-  TwitterLogoIcon,
-  GlobeIcon
-} from '@radix-ui/react-icons'
+import { twMerge } from 'tailwind-merge'
 
 const TeamSection = () => {
   const Teams = [
@@ -80,42 +81,42 @@ const TeamSection = () => {
   return (
     <section
       className={twMerge(
-        `text-gray-700 bg-[#fdfdfd] min-h-screen flex flex-col gap-6 w-full py-12 px-6`
+        `text-gray-700 bg-[#fdfdfd] min-h-screen flex flex-col gap-6 w-full py-12 md:px-[7.25rem]`
       )}
     >
-      <h1 className='text-4xl md:text-5xl text-center font-bold font-poppins '>
+      <h1 className='text-4xl font-bold text-center md:text-5xl font-poppins '>
         Meet the Team
       </h1>
-      <span className='text-lg md:text-xl text-center font-poppins pb-18'>
+      <span className='text-lg text-center md:text-xl font-poppins pb-18'>
         Behind every greate event is a dedicated team. Get to know the
         passionate individuals driving this experience, each commmitted to
         making a lasting impact and bringing innovative ideas to life
       </span>
 
-      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+      <div className='grid w-full grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {Teams.map((team, index) => (
           <div
             key={index}
-            className='bg-white flex flex-col items-start text-left h-full gap-2'
+            className='flex flex-col items-start h-full gap-2 text-left bg-white'
           >
             <img
               src={team.teamImg}
               alt={team.role}
-              className='w-full h-full object-cover mb-4'
+              className='object-cover w-full h-full mb-4'
             />
             <h3 className='text-2xl font-semibold text-gray-900'>
               {team.name}
             </h3>
             <p className='text-lg text-gray-500'>{team.role}</p>
-            <p className='text-lg text-gray-400 mt-2 mb-4'>
+            <p className='mt-2 mb-4 text-lg text-gray-400'>
               {team.description}
             </p>
-            <div className='mt-auto flex gap-3'>
+            <div className='flex gap-3 mt-auto'>
               <a
                 href={team.LinkedInURL}
                 target='_blank'
                 rel='noopener noreferrer'
-                className='text-blue-400 hover:text-blue-600 transition'
+                className='text-blue-400 transition hover:text-blue-600'
               >
                 <TwitterLogoIcon className='w-9 h-9' />
               </a>
@@ -123,7 +124,7 @@ const TeamSection = () => {
                 href={team.LinkedInURL}
                 target='_blank'
                 rel='noopener noreferrer'
-                className='text-gray-500 hover:text-blue-800 transition'
+                className='text-gray-500 transition hover:text-blue-800'
               >
                 <LinkedInLogoIcon className='w-9 h-9' />
               </a>
@@ -131,7 +132,7 @@ const TeamSection = () => {
                 href={team.LinkedInURL}
                 target='_blank'
                 rel='noopener noreferrer'
-                className='text-gray-500 hover:text-blue-800 transition'
+                className='text-gray-500 transition hover:text-blue-800'
               >
                 <GlobeIcon className='w-9 h-9' />
               </a>
